af1fe5a8da1518b3fcabbdbb86b744ad6eff8bf5
eshandawadi <eshandawadi123@gmail.com>
2022-01-18 15:19:19 +0545

    sorting

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// var array = ['ram', 'White', 'White flower', 'shyam'];
+
+// array.sort(function(x, y) {
+//   if (x < y) {
+//     return -1;
+//   }
+//   if (x > y) {
+//     return 1;
+//   }
+//   return 0;
+// });
